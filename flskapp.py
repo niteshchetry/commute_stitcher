da3130854b0f3751f76fc9497a6abc920e8ec55b
niteshchetry <chetrynitesh@gmail.com>
2018-12-15 00:06:42 -0800

    updated flskapp to work

--- flskapp.py
+++ flskapp.py
@@ -1,8 +1,8 @@
 from flask import Flask
-import route_calculator
+import route_planner
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return route_calculator.fastest_route('10801 Harbour Pointe Blvd, Mukilteo, WA 98275', '4069 Spokane Ln, Seattle, WA 98105')
+    return route_planner.fastest_route('10801 Harbour Pointe Blvd, Mukilteo, WA 98275', '4069 Spokane Ln, Seattle, WA 98105')
