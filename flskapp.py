0fe2578130c58afff5db66ccfe392199cc72d3a3
niteshchetry <chetrynitesh@gmail.com>
2018-12-15 00:06:42 -0800

    updated flskapp to work

--- flskapp.py
+++ flskapp.py
@@ -1,8 +1,8 @@
 from flask import Flask
-import route_calculator
+import route_planner
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return route_calculator.fastest_route('***REMOVED***', '***REMOVED***')
+    return route_planner.fastest_route('***REMOVED***', '***REMOVED***')
