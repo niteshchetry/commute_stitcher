fd62e07f7338b6e8c81ba891746c3ce0625b1323
niteshchetry <chetrynitesh@gmail.com>
2018-12-17 19:57:16 -0800

    added front end to the application using html, css, and js. Also added an embedded google map onto the page for visuals

--- flskapp.py
+++ flskapp.py
@@ -1,8 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import route_planner
+import config
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return route_planner.fastest_route('10801 Harbour Pointe Blvd, Mukilteo, WA 98275', '4069 Spokane Ln, Seattle, WA 98105')
+    return render_template('home.html', gmaps_api_key = config.gmaps_api_key)
+
+@app.route('/', methods = ['POST'])
+def result():
+    origin = request.form['origin']
+    destinaton = request.form['destination']
+    ordered_routes = route_planner.fastest_route(origin, destinaton,)
+    return render_template('result.html', ordered_routes = ordered_routes, gmaps_api_key = config.gmaps_api_key)
+
+if __name__ == '__main__':
+    app.run(debug=False)
